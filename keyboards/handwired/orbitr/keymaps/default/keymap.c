3f765f2f5a3336ce68667022c4246bff42214be6
Olivier Peyrusse <ope@activeviam.com>
2023-07-12 22:56:29 +0200

    Customize a bit for tests.

--- keyboards/handwired/orbitr/keymaps/default/keymap.c
+++ keyboards/handwired/orbitr/keymaps/default/keymap.c
@@ -7,47 +7,34 @@ enum custom_keycodes {
     KC_P00 = SAFE_RANGE
 };
 
+enum layers {
+    BASE,
+    UP
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │TG1│ / │ * │ - │
-     * ├───┼───┼───┼───┤
-     * │ 7 │ 8 │ 9 │ + │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ % │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ = │
-     * ├───┼───┼───┼───┤
-     * │ 0 │00 │ . │Ent│
-     * └───┴───┴───┴───┘
+     * ┌───┬───┬───┐
+     * │MO1│ / │ - │
+     * ├───┼───┼───┤
+     * │ 1 │ 2 │ 3 │
+     * └───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_5x4(
-        TG(1),   KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PERC,
-        KC_P1,   KC_P2,   KC_P3,   KC_EQL,
-        KC_P0,   KC_P00,  KC_PDOT, KC_PENT
+    [BASE] = LAYOUT(
+        MO(UP), KC_SLASH, KC_MINUS,
+        KC_1  , KC_2    , KC_3
     ),
 
     /*
-     * ┌───┬───┬───┬───┐
-     * │TG1│ / │ * │ - │
-     * ┌───┬───┬───┐───┤
-     * │Hom│ ↑ │PgU│ + │
-     * ├───┼───┼───┤───┤
-     * │ ← │   │ → │ % │
-     * ├───┼───┼───┤───┤
-     * │End│ ↓ │PgD│ = │
-     * ├───┼───┼───┤───┤
-     * │Ins│   │Del│Ent│
-     * └───┴───┴───┘───┘
+     * ┌───┬───┬───┐
+     * │MO1│ / │   │
+     * ├───┼───┼───┤
+     * │ a │ b │ c │
+     * └───┴───┴───┘
      */
-    [1] = LAYOUT_ortho_5x4(
-        _______, _______, _______, _______,
-        KC_HOME, KC_UP,   KC_PGUP, _______,
-        KC_LEFT, XXXXXXX, KC_RGHT, _______,
-        KC_END,  KC_DOWN, KC_PGDN, _______,
-        KC_INS,  XXXXXXX, KC_DEL,  _______
+    [UP] = LAYOUT(
+        _______, KC_EQL , XXXXXXX,
+        KC_A   , KC_B   , KC_C
     )
 };
 
