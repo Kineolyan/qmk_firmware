82981f4d714c8041399a40e165f6f3a85d7ad5ed
Kineolyan <kineolyan@protonmail.com>
2023-08-31 21:25:05 +0200

    Correct the basic layout.

--- keyboards/handwired/splx/full/keymaps/default/keymap.c
+++ keyboards/handwired/splx/full/keymaps/default/keymap.c
@@ -21,9 +21,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┘
      */
     [BASE] = LAYOUT(
-                  KC_SCLN , KC_COMM , KC_DOT  , KC_P    , KC_Y    ,         ,           KC_F    , KC_G    , KC_C    , KC_R    , KC_L    ,
-        KC_EQL  , KC_A    , KC_O    , KC_E    , KC_U    , KC_I    ,         ,         , KC_D    , KC_H    , KC_T    , KC_N    , KC_S    , KC_MINS   ,
-        KC_SLSH , KC_QUOT , KC_Q    , KC_J    , KC_K    , KC_X    ,         ,         , KC_B    , KC_M    , KC_W    , KC_V    , KC_Z    , S(KC_EQL) ,
+                  KC_SCLN , KC_COMM , KC_DOT  , KC_P    , KC_Y                        , KC_F    , KC_G    , KC_C    , KC_R    , KC_L    ,
+        KC_EQL  , KC_A    , KC_O    , KC_E    , KC_U    , KC_I                        , KC_D    , KC_H    , KC_T    , KC_N    , KC_S    , KC_MINS   ,
+        KC_SLSH , KC_QUOT , KC_Q    , KC_J    , KC_K    , KC_X                        , KC_B    , KC_M    , KC_W    , KC_V    , KC_Z    , S(KC_EQL) ,
                   KC_LCTL , KC_LALT , KC_LSFT , KC_LGUI , KC_BSPC , KC_ENTER, KC_SPC  , KC_TAB  , KC_RGUI , KC_RSFT , KC_RALT , KC_RCTL
     )
 };
