a9b502a83242edef9314f42df5886b16aec92f31
Kineolyan <kineolyan@protonmail.com>
2024-11-11 15:35:09 +0100

    Add keymap for debug

--- keyboards/handwired/splx/full/keymaps/dbg/keymap.c
+++ keyboards/handwired/splx/full/keymaps/dbg/keymap.c
@@ -1,5 +1,6 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
+// Debug mapping with only two keys (thumbs)
 
 #include QMK_KEYBOARD_H
 
@@ -9,7 +10,6 @@ enum custom_keycodes {
 
 enum layers {
     BASE,
-    UP
 };
 
 enum td_keycodes {
@@ -19,8 +19,6 @@ enum td_keycodes {
 typedef enum {
     TD_NONE,
     TD_UNKNOWN,
-    TD_SINGLE_TAP,
-    TD_SINGLE_HOLD,
 } td_state_t;
 
 // Function to determine the current tapdance state
@@ -31,30 +29,10 @@ void sftqm_finished(tap_dance_state_t *state, void *user_data);
 void sftqm_reset(tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┐
-     * │MO1│ / │ - │
-     * ├───┼───┼───┤
-     * │ 1 │ 2 │ 3 │
-     * └───┴───┴───┘
-     */
-    [BASE] = LAYOUT_flat(
-                  KC_SCLN , KC_COMM , KC_DOT  , KC_P    , KC_Y                        , KC_F    , KC_G    , KC_C    , KC_R    , KC_L    ,
-        KC_EQL  , KC_A    , KC_O    , KC_E    , KC_U    , KC_I                        , KC_D    , KC_H    , KC_T    , KC_N    , KC_S    , KC_MINS   ,
-        KC_SLSH , KC_QUOT , KC_Q    , KC_J    , KC_K    , KC_X                        , KC_B    , KC_M    , KC_W    , KC_V    , KC_Z    , S(KC_EQL) ,
-                  KC_LCTL , KC_LALT , KC_LSFT , KC_LGUI , KC_BSPC , KC_ENTER, KC_SPC  , KC_TAB  , KC_RGUI , KC_RSFT , KC_RALT , KC_RCTL
-    )
+    [BASE] = LAYOUT_dbg(KC_L, KC_R)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch(keycode) {
-            case KC_P00:
-                tap_code(KC_P0);
-                tap_code(KC_P0);
-                return false;
-        }
-    }
     return true;
 }
 
